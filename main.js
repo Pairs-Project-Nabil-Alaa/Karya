51f61ecd9abfeb874391534c0fe3a6ec97b355bd
ALAAMEKKI <alaamekki01@gmail.com>
2020-12-18 11:46:27 +0100

    init

--- main.js
+++ main.js
@@ -1,29 +1,60 @@
 //console.log('test')
 $(document).ready(function () {
 
-    $('.modal').hide()
+    $('#forma').hide()
     
-    $('#signup-button').click(function(){
-        $('.modal').show()
+    $('#login').click(function(){
+        $('#forma').show()
+        
 
     })
     
-
-     var sel = document.getElementsByClassName('search-selector')
-     
-     if (sel.option[1].selected === true){
-        $('.search-btn').click(function(){
-
-            alert('bizerte')
-        })
+    // Select Button//////////////////////////////////////////////////////////////////////////////////////////////////////////////////
     
-   
-}
+    //  var sel = document.getElementsByClassName('search-selector')
+     
+    //  if (sel.option[1].selected === true){
+    //     $('.search-btn').click(function(){
 
+    //         alert('bizerte')
+    //     })
+    //  }
+   
+    $(function () {
+        $(".search-btn").click(function () {
+            var selectedText = $(".search-selector").find("option:selected").text();
+            if(selectedText === "Bizerte"){
+                alert('Welcome to Bizerte')
+            }
+            
+        });
+    });
+
+
+    $(function () {
+        $(".search-btn").click(function () {
+            var selectedText = $(".search-selector").find("option:selected").text();
+            if(selectedText === "Tunis"){
+                alert('Tunis will be available soon..')
+            }
+            
+        });
+    });
+
+    $(function () {
+        $(".search-btn").click(function () {
+            var selectedText = $(".search-selector").find("option:selected").text();
+            if(selectedText === "Nabeul"){
+                alert('Nabeul will be available soon..')
+            }
+            
+        });
+    });
+    
 })
 
 
 
-
+//log in /////////////////////////////////////////////////////////////////////////////////////////////////////////////
 
 
